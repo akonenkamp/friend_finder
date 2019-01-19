e64514cdca01b2be72c2708219b1317d8f8463dd
Konenkamp <n0267144@lm.lmig.com>
2019-01-18 20:41:31 -0800

    Adding friend js with dummy data

---
+++ data/friends.js
@@ -0,0 +1,21 @@
+//array that holds all the 'friends', initialized with dummy data
+var friendList = [
+    {
+        name: 'Amanda Konenkamp',
+        profilePic: 'https://goo.gl/images/UkNpjV',
+        scores: [
+            "1",
+            "4",
+            "5",
+            "2",
+            "2",
+            "5",
+            "4",
+            "4",
+            "5",
+            "5"
+        ]
+    }
+]
+
+module.exports = friendList;
