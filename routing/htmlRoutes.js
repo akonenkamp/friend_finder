e337fd160649c9525ab922a0a3b77202ae6c6c0c
Konenkamp <n0267144@lm.lmig.com>
2019-01-21 23:25:47 -0800

    not functioning but fixing git ignore

---
+++ routing/htmlRoutes.js
@@ -0,0 +1,16 @@
+//Dependencies
+var path = require('path');
+
+//ROUTING
+
+module.exports = function (app) {
+    //default GET route that leads to home.html - displays home page
+    app.get('/survey', function (req, res) {
+        res.sendFile(path.join(__dirname + '/../survey.html'));
+    });
+
+    // to home page
+    app.use(function (req, res) {
+        res.sendFile(path.join(__dirname + '/../home.html'));
+    });
+};
